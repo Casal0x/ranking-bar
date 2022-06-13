405323b0176e9dc19b8b7dcd71f6fe8f08adc808
Pato Barbero <patogbarbero@gmail.com>
2022-06-13 17:38:41 -0300

    Agregamos bootstrap

--- src/layouts/MainLayout/index.jsx
+++ src/layouts/MainLayout/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../../components/Header";
 
 const MainLayout = () => {
   return (
     <div className="main-layout">
-      Este es el main layout
+      <Header />
       <div className="routes">
         <Outlet />
       </div>
